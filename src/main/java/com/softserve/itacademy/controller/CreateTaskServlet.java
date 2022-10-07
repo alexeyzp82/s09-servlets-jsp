517a1f5d7dddd7735d2c055014c4000faa70230c
Bohdan Bazyl <pooplegenry@gmail.com>
2022-10-07 23:26:52 +0300

    add WebServlet annotations for CreateTaskServlet and TaskListServlet classes

--- src/main/java/com/softserve/itacademy/controller/CreateTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/CreateTaskServlet.java
@@ -2,10 +2,11 @@ package com.softserve.itacademy.controller;
 
 import com.softserve.itacademy.repository.TaskRepository;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet(value = {"/create-task"})
 public class CreateTaskServlet extends HttpServlet {
 
     private TaskRepository taskRepository;
