a4d8a104bb03b2e8f1199bdad20047429c50d1c0
Bohdan Bazyl <pooplegenry@gmail.com>
2022-10-08 01:29:03 +0300

    developed tasks-list page

--- src/main/java/com/softserve/itacademy/controller/TasksListServlet.java
+++ src/main/java/com/softserve/itacademy/controller/TasksListServlet.java
@@ -2,10 +2,14 @@ package com.softserve.itacademy.controller;
 
 import com.softserve.itacademy.repository.TaskRepository;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @WebServlet(value = {"/tasks-list"})
 public class TasksListServlet extends HttpServlet {
 
@@ -16,8 +20,10 @@ public class TasksListServlet extends HttpServlet {
         taskRepository = TaskRepository.getTaskRepository();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/tasks-list.jsp");
+        request.setAttribute("tasks", taskRepository.all());
+        requestDispatcher.forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
--- src/main/webapp/WEB-INF/pages/tasks-list.jsp
+++ src/main/webapp/WEB-INF/pages/tasks-list.jsp
@@ -12,8 +12,34 @@
 </head>
 <body>
     <%@include file="header.html"%>
+    <h2>List of Tasks</h2>
+    <table border="1">
+        <tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>Priority</th>
+            <th colspan="3">Operation</th>
+        </tr>
 
-
+        <%
+            for (Task task: (List<Task>)request.getAttribute("tasks")) {
+        %>
+        <tr>
+            <td><%=task.getId()%></td>
+            <td><%=task.getTitle()%></td>
+            <td><%=task.getPriority()%></td>
+            <td>
+                <a href="/read-task?id=<%=task.getId()%>">Read</a>
+            </td>
+            <td>
+                <a href="/edit-task?id=<%=task.getId()%>">Edit</a>
+            </td>
+            <td><a href="/delete-task?id=<%=task.getId()%>">Delete</a></td>
+        </tr>
+        <%
+            }
+        %>
+    </table>
 
 </body>
 </html>
