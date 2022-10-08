845d4c2f719ec1e7734e1db029b3f20525ccb964
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2022-10-09 02:03:42 +0300

    alex009

--- src/main/java/com/softserve/itacademy/controller/UpdateTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/UpdateTaskServlet.java
@@ -1,25 +1,40 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.Priority;
+import com.softserve.itacademy.model.Task;
 import com.softserve.itacademy.repository.TaskRepository;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(value = {"/update-task"})
 public class UpdateTaskServlet extends HttpServlet {
 
     private TaskRepository taskRepository;
+    private Task task;
 
     @Override
     public void init() {
         taskRepository = TaskRepository.getTaskRepository();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        task = taskRepository.read(id);
+        request.setAttribute("task", task);
+        request.getRequestDispatcher("/WEB-INF/pages/edit-task.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String title = request.getParameter("title");
+        Priority priority = Priority.valueOf(request.getParameter("priority").toUpperCase());
+        task.setTitle(title);
+        task.setPriority(priority);
+        taskRepository.update(task);
+        response.sendRedirect("/tasks-list");
     }
 }
--- src/main/webapp/WEB-INF/pages/edit-task.jsp
+++ src/main/webapp/WEB-INF/pages/edit-task.jsp
@@ -11,8 +11,40 @@
 
 </head>
 <body>
-    <%@include file="header.html"%>
+<%@include file="header.html" %>
+<h2>Edit existing Task</h2>
 
+<%
+    Task task = (Task) request.getAttribute("task");
+%>
+
+<form method="post">
+    <div>
+        <label for="id">Name: </label>
+        <input name="id" id="id" value="<%=task.getId()%>" disabled>
+    </div>
+    <div>
+        <label for="title">Name: </label>
+        <input name="title" id="title" value="<%=task.getTitle()%>">
+    </div>
+    <div>
+        <label for="priority">Priority: </label>
+        <select name="priority" id="priority">
+            <%
+                for (Priority priority : Priority.values()) {
+            %>
+            <option name=<%=priority.name()%>><%=priority.info()%>
+            </option>
+            <%
+                }
+            %>
+        </select>
+    </div>
+    <div>
+        <button type="submit">Update</button>
+        <button type="reset">Clear</button>
+    </div>
+</form>
 
 
 </body>
