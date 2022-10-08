f557cab2f39868d5949940839e9ce033efdb61c9
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2022-10-09 02:39:58 +0300

    Everything works, but not all tests pass (8/17)

--- src/main/java/com/softserve/itacademy/controller/DeleteTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/DeleteTaskServlet.java
@@ -2,10 +2,14 @@ package com.softserve.itacademy.controller;
 
 import com.softserve.itacademy.repository.TaskRepository;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(value = {"/delete-task"})
 public class DeleteTaskServlet extends HttpServlet {
 
     private TaskRepository taskRepository;
@@ -15,8 +19,12 @@ public class DeleteTaskServlet extends HttpServlet {
         taskRepository = TaskRepository.getTaskRepository();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if(taskRepository.delete(Integer.parseInt(request.getParameter("id")))) {
+            response.sendRedirect("/tasks-list");
+        } else {
+            response.sendError(404);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
--- src/main/webapp/WEB-INF/pages/error.jsp
+++ src/main/webapp/WEB-INF/pages/error.jsp
@@ -1,3 +1,7 @@
+<%@ page import="com.softserve.itacademy.model.Task" %>
+<%@ page import="com.softserve.itacademy.controller.ReadTaskServlet" %>
+<%@ page import="javax.servlet.annotation.WebServlet" %>
+<%@ page import="com.softserve.itacademy.controller.TasksListServlet" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <!doctype html>
 <html lang="en">
@@ -11,8 +15,7 @@
 
 </head>
 <body>
-
-
-
+    <%@include file="header.html"%>
+    <h2>Task with ID '<%=request.getParameter("id")%>' not found in To-Do List!</h2>
 </body>
 </html>
