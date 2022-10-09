40cca2c18c0887d8a92cabc3c08e0730c17daac8
Bohdan Bazyl <pooplegenry@gmail.com>
2022-10-09 11:32:43 +0300

    edit-task works, but not passing tests

--- src/main/java/com/softserve/itacademy/controller/UpdateTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/UpdateTaskServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 @WebServlet(value = {"/update-task"})
 public class UpdateTaskServlet extends HttpServlet {
@@ -25,16 +26,30 @@ public class UpdateTaskServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("id"));
         task = taskRepository.read(id);
-        request.setAttribute("task", task);
-        request.getRequestDispatcher("/WEB-INF/pages/edit-task.jsp").forward(request, response);
+        if (task != null) {
+            request.setAttribute("task", task);
+            request.getRequestDispatcher("/WEB-INF/pages/edit-task.jsp").forward(request, response);
+        } else {
+            request.setAttribute("pageURL", "/update-task");
+            response.sendError(404);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String title = request.getParameter("title");
         Priority priority = Priority.valueOf(request.getParameter("priority").toUpperCase());
-        task.setTitle(title);
+        if(!title.isBlank()) {
+            task.setTitle(title);
+        } else {
+            request.setAttribute("pageURL", "/update-task");
+            response.sendError(404);
+        }
         task.setPriority(priority);
-        taskRepository.update(task);
-        response.sendRedirect("/tasks-list");
+        if (taskRepository.update(task)) {
+            response.sendRedirect("/tasks-list");
+        } else {
+            request.setAttribute("pageURL", "/update-task");
+            response.sendError(404);
+        }
     }
 }
